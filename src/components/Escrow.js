6c9b474700fc91c1d9fab3e5db86536691d405b7
cartoonweber <minkbuckstar@gmail.com>
2022-04-02 05:44:46 +1000

    listen event fix

--- src/components/Escrow.js
+++ src/components/Escrow.js
@@ -8,14 +8,17 @@ export default function EscrowContainer({address, scanner, rpcUrl}) {
     const [latestEscrow, setLatestEscrow] = useState('');
     const [count, setCount] = useState(0);
     const eventsUrl = `${scanner}/address/${address}#events`;
-    const web3 = getWeb3(rpcUrl);
-    const EscrowDefaultFactory = new web3.eth.Contract(EscrowFactoryABI, localStorage.getItem("defaultAddr"));
-    console.log("contract", EscrowDefaultFactory.events);
-    // EscrowDefaultFactory.events.
-
+    
     useEffect(() => {
+        
         async function setupEscrow() {
             try {
+                const web3 = getWeb3(rpcUrl);
+                const EscrowDefaultFactory = new web3.eth.Contract(EscrowFactoryABI, localStorage.getItem("defaultAddr"));
+                console.log("contract", EscrowDefaultFactory.events);
+                EscrowDefaultFactory.events.allEvents().on('Launched', (e) => {
+                    console.log("event ", e);
+                })
                 const EscrowFactory = new web3.eth.Contract(EscrowFactoryABI, address);
                 const escrowCount = await EscrowFactory.methods.counter().call();
                 setCount(escrowCount);
