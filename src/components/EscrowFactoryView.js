edb50f6cb58604aa083c6038ef82c4248d3e0b7d
cartoonweber <minkbuckstar@gmail.com>
2022-04-02 23:52:38 +1000

    frontend doen

--- src/components/EscrowFactoryView.js
+++ src/components/EscrowFactoryView.js
@@ -19,56 +19,26 @@ import {
   TablePagination,
 } from "@mui/material";
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
+function createData(escrow, childCounter, duration) {
+  return { escrow, childCounter, duration};
 }
 
 export default function Escrow(props) {
   const { count, address, latestEscrow, eventsUrl, scanner } = props;
 
   const columns = [
-    { id: "name", label: "Name", minWidth: 170 },
-    { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
+    { id: "escrow", label: "Escrow", minWidth: 170 },
+    { id: "childCounter", label: "Child Counter", minWidth: 100 },
     {
-      id: "population",
-      label: "Population",
+      id: "duration",
+      label: "Duaration",
       minWidth: 170,
-      align: "right",
       format: (value: number) => value.toLocaleString("en-US"),
-    },
-    {
-      id: "size",
-      label: "Size\u00a0(km\u00b2)",
-      minWidth: 170,
-      align: "right",
-      format: (value: number) => value.toLocaleString("en-US"),
-    },
-    {
-      id: "density",
-      label: "Density",
-      minWidth: 170,
-      align: "right",
-      format: (value: number) => value.toFixed(2),
-    },
+    }
   ];
 
   const rows = [
-    createData("India", "IN", 1324171354, 3287263),
-    createData("China", "CN", 1403500365, 9596961),
-    createData("Italy", "IT", 60483973, 301340),
-    createData("United States", "US", 327167434, 9833520),
-    createData("Canada", "CA", 37602103, 9984670),
-    createData("Australia", "AU", 25475400, 7692024),
-    createData("Germany", "DE", 83019200, 357578),
-    createData("Ireland", "IE", 4857000, 70273),
-    createData("Mexico", "MX", 126577691, 1972550),
-    createData("Japan", "JP", 126317000, 377973),
-    createData("France", "FR", 67022000, 640679),
-    createData("United Kingdom", "GB", 67545757, 242495),
-    createData("Russia", "RU", 146793744, 17098246),
-    createData("Nigeria", "NG", 200962417, 923768),
-    createData("Brazil", "BR", 210147125, 8515767),
+    createData("0x45eBc3eAE6DA485097054ae10BA1A0f8e8c7f794", 354, 32873),
   ];
 
   const [page, setPage] = React.useState(0);
