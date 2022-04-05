6a473682f5e47504a9a2d0284e9bf749b6db559b
cartoonweber <minkbuckstar@gmail.com>
2022-04-06 06:29:03 +1000

    fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,67 @@
+import { useState } from 'react';
 import { Typography } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
-export default function Escrow(props) {
+export default function Footer(props) {
+  const[email, setEmail] = useState("");
   return (
-    <Card
-      className="footer-container"
-      sx={{
-        width: "100%",
-        bottom: "0px",
-        left: "0px",
-      }}
-    >
-      <CardContent>
-        <CardTextBlock
-          value={
-            <Link
-              href="https://github.com/humanprotocol/hmt-escrow"
-              target="_blank"
-              rel="noreferrer"
+    <Box sx={{padding: '100px 60px'}}>
+      <Stack
+          direction={{ lg: "row", md: "row", sm: "column", xs: "column" }}
+          spacing={2}
+          sx={{
+            width: "100%",
+          }}
+        >
+          <Box>
+            <TextField
+              variant="outlined"
+              size="small"
+              label="Email"
+              sx={{width: "calc(100%)",  flexBasis: "100%"}}
+              onChange={(e) => setEmail(e.target.value)}
             >
-              HMT Escrow Source Code
-            </Link>
-          }
-        />
-      </CardContent>
-    </Card>
+              {email}
+            </TextField>
+            <Button
+              sx={{backgroundColor: '#320A8D', color: 'white', textTransform: 'capitalize'}}
+            >
+                Subscribe
+            </Button>
+          </Box>
+          <Box>
+
+          </Box>
+      </Stack>
+    </Box>
+    // <Card
+    //   className="footer-container"
+    //   sx={{
+    //     width: "100%",
+    //     bottom: "0px",
+    //     left: "0px",
+    //   }}
+    // >
+    //   <CardContent>
+    //     <CardTextBlock
+    //       value={
+    //         <Link
+    //           href="https://github.com/humanprotocol/hmt-escrow"
+    //           target="_blank"
+    //           rel="noreferrer"
+    //         >
+    //           HMT Escrow Source Code
+    //         </Link>
+    //       }
+    //     />
+    //   </CardContent>
+    // </Card>
   );
 }
 
