2f0f5435c6882d8d34921e52f52d3205b5b8e727
cartoonweber <minkbuckstar@gmail.com>
2022-04-06 17:32:28 +1000

    mailchamp fix

--- src/components/CustomForm.js
+++ src/components/CustomForm.js
@@ -5,8 +5,36 @@ import Stack from "@mui/material/Stack";
 
 const CustomForm = ({ status, message, onValidated }) => {
     const [email, setEmail] = useState('');
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      email
+      email.indexOf("@") > -1 &&
+      onValidated({
+          EMAIL: email,
+      });
+    }
+
     return (
-      <form className="mc__form">
+      <form className="mc__form" onSubmit={(e) => handleSubmit(e)}>
+        {status === "sending" && (
+          <div className="mc__alert mc__alert--sending">
+            sending...
+          </div>
+        )}
+        {status === "error" && (
+          <div 
+            className="mc__alert mc__alert--error"
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}
+        {status === "success" && (
+          <div
+            className="mc__alert mc__alert--success"
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}
+        
         <Stack
             direction={{ lg: "row", md: "row", sm: "row", xs: "column" }}
             spacing={2}
@@ -23,7 +51,7 @@ const CustomForm = ({ status, message, onValidated }) => {
                 onChange={(e) => setEmail(e.target.value)}
               >
                 {email}
-              </TextField>
+            </TextField>
           </div>
             <Button
               type="submit"
