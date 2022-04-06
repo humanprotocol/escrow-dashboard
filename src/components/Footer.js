48be2809e21c248e81da4dd30bcd9b29426af8ca
cartoonweber <minkbuckstar@gmail.com>
2022-04-06 15:42:00 +1000

    feature/escrow

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,11 @@ import Stack from "@mui/material/Stack";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
+import gitImg from '../assets/images/git.png';
+import discordImg from '../assets/images/discord.png';
+import inImg from '../assets/images/in.png';
+import twitterImg from '../assets/images/twitter.png';
+
 export default function Footer(props) {
   const[email, setEmail] = useState("");
   return (
@@ -15,28 +20,137 @@ export default function Footer(props) {
       <Stack
           direction={{ lg: "row", md: "row", sm: "column", xs: "column" }}
           spacing={2}
+          justifyContent="space-between"
           sx={{
             width: "100%",
           }}
         >
           <Box>
-            <TextField
-              variant="outlined"
-              size="small"
-              label="Email"
-              sx={{width: "calc(100%)",  flexBasis: "100%"}}
-              onChange={(e) => setEmail(e.target.value)}
+            <Stack
+              direction={{ lg: "row", md: "row", sm: "row", xs: "column" }}
+              spacing={2}
+              sx={{
+                width: "100%",
+              }}
             >
-              {email}
-            </TextField>
-            <Button
-              sx={{backgroundColor: '#320A8D', color: 'white', textTransform: 'capitalize'}}
-            >
-                Subscribe
-            </Button>
+              <TextField
+                variant="outlined"
+                size="small"
+                label="Email"
+                sx={{width: "calc(100%)",  flexBasis: "100%"}}
+                onChange={(e) => setEmail(e.target.value)}
+              >
+                {email}
+              </TextField>
+              <Button
+                sx={{backgroundColor: '#320A8D', color: 'white', textTransform: 'capitalize', padding:'0 50px', marginLeft: '20px'}}
+              >
+                  Subscribe
+              </Button>
+            </Stack>
+            <Stack>
+              <ul className="footer-img-link">
+                <li style={{marginRight: '20px', marginLeft: '0px'}}>
+                  <a href="https://" target="_blank">
+                    <img src={gitImg} />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://" target="_blank">
+                    <img src={discordImg}/>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://" target="_blank">
+                    <img src={twitterImg}/>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://" target="_blank">
+                    <img src={inImg}/>
+                  </a>
+                </li>
+              </ul>
+            </Stack>
           </Box>
           <Box>
+            <Stack
+              direction={{ lg: "row", md: "row", sm: "row", xs: "column" }}
+              spacing={2}
+              sx={{
+                width: "100%"
+              }}
+            >
+              <ul className="footer-text-link">
+                <li>
+                  <a>
+                    Lightpaper
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Whitepaper
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    AI Whitepater
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Token
+                  </a>
+                </li>
+              </ul>
 
+              <ul className="footer-text-link">
+                <li>
+                  <a>
+                    Technology
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Developers
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    GitHub
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Blog
+                  </a>
+                </li>
+              </ul>
+
+              <ul className="footer-text-link">
+                <li>
+                  <a>
+                    Ambassador Hub
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Brand Assets
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Team
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    Carreers
+                  </a>
+                </li>
+              </ul>
+
+            </Stack>
           </Box>
       </Stack>
     </Box>
