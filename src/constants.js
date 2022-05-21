47b5b73b1b1b237b22b31533681f54f7155ef651
Paul <41552663+molecula451@users.noreply.github.com>
2022-05-21 08:43:21 -0700

    add factory

--- src/constants.js
+++ src/constants.js
@@ -13,5 +13,12 @@ export const networkMap = {
     rpcUrl: 'https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
     defaultFactoryAddr: '0x925B24444511c86F4d4E63141D8Be0A025E2dca4'
   },
+  neonslabdev: {
+    title: 'NeonsLab Devtestnet',
+    key: 'neonslabdev',
+    scanner: 'https://neonscan.org',
+    rpcUrl: 'https://proxy.devnet.neonlabs.org/solana',
+    defaultFactoryAddr: '0xB46b95aa85D2085Bf6CCbb9cB953c28cc7DE6F4a'
+  }
 }
 export const networks = Object.values(networkMap).map(network => network);
