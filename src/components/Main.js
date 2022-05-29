ff28d3cdee8b609c29600d8a5156498e905da466
Jalal-1 <jalal@hmt.ai>
2022-05-29 17:35:52 +0100

    Added Metamask as auth context provider

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,31 @@
 import { useContext, useState } from "react";
 import Box from "@mui/material/Box";
-
+import Button from "@mui/material/Button";
 import Header from "./Header";
 import Escrow from "./Escrow";
 import Search from "./Search";
 import NetworkSwitcher from "./NetworkSwitcher";
 import Footer from "./Footer";
-
+import useMetaMask from "../hooks/metamask";
 import { networkMap } from "../constants";
 import AppContext from "../AppContext";
 
 function Main() {
   const { network, setNetwork } = useContext(AppContext);
-
+  const { connect, disconnect, isActive, account, shouldDisable } =
+    useMetaMask();
   const [escrowFactory, setEscrowFactory] = useState(
     networkMap[network].defaultFactoryAddr
   );
 
-  const onNetworkChange = networkKey => {
+  const onNetworkChange = (networkKey) => {
     setNetwork(networkKey);
     setEscrowFactory(networkMap[networkKey].defaultFactoryAddr);
   };
   return (
     <Box>
       <Header />
+
       <Box
         sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
       >
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Web3 from "web3";
+import { Web3ReactProvider } from "@web3-react/core";
+import { MetaMaskProvider } from "./hooks/metamask";
+
+function getLibrary(provider, connector) {
+  return new Web3(provider);
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <MetaMaskProvider>
+        <App />
+      </MetaMaskProvider>
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
