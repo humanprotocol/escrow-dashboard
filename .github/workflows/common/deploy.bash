733ee12822926d8a31404c9f39d4c15ad5fccffd
Your Name <you@example.com>
2022-06-02 15:02:37 -0300

    Docker login didn't work properly for some reason, found a way which works

--- .github/workflows/common/deploy.bash
+++ .github/workflows/common/deploy.bash
@@ -5,7 +5,19 @@ set -e # quit on error with last exit code
 set -eo pipefail
 
 readonly IMAGE="$1"
+#readonly REPO="${IMAGE%%:*}"
 readonly CONTAINER_NAME="escrow-dashboard"
+readonly AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+readonly REGION="$(echo "$AVAIL_ZONE" | sed 's/[a-z]$//g')"
+readonly ACC_ID=$(aws sts get-caller-identity --query "Account" --output text)
+readonly REGISTRY=$ACC_ID.dkr.ecr.$REGION.amazonaws.com
+
+if ! grep -q "${REGISTRY}" ~/.docker/config.json ; then
+        #readonly TOKEN=$(aws ecr get-authorization-token --region "${REGION}" --output text --query 'authorizationData[].authorizationToken')
+        #curl -i -H "Authorization: Basic $TOKEN" https://"${REGISTRY}"/v2/amazonlinux/tags/list
+        aws ecr get-login-password --region "${REGION}" | docker login -u AWS "${REGISTRY}" --password-stdin
+        #aws ecr get-login-password --region "${REGION}" | docker login -u AWS "${REPO}" -p
+fi
 
 docker pull $IMAGE || echo "Failed to pull docker image $IMAGE"
 
