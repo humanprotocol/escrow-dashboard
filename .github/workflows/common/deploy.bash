3e71cd4fcabf9e620f8670b71f51ed14ca1ade11
Ivan <ivan@hmt.ai>
2022-06-05 22:37:20 -0300

    Revert "Remove docker login from deploy script" in order to work even if ~/.docker/config.json wasn't preconfigured
    This reverts commit a343e42ba3a0af0a0fc33ea2ccfe7c490a598fb8.

--- .github/workflows/common/deploy.bash
+++ .github/workflows/common/deploy.bash
@@ -5,7 +5,29 @@ set -e # quit on error with last exit code
 set -eo pipefail
 
 readonly IMAGE="$1"
+#readonly REPO="${IMAGE%%:*}"
 readonly CONTAINER_NAME="escrow-dashboard"
+readonly AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+readonly REGION="$(echo "$AVAIL_ZONE" | sed 's/[a-z]$//g')"
+readonly ACC_ID=$(aws sts get-caller-identity --query "Account" --output text)
+readonly REGISTRY=$ACC_ID.dkr.ecr.$REGION.amazonaws.com
+
+function dockerlogin() {
+        #readonly TOKEN=$(aws ecr get-authorization-token --region "${REGION}" --output text --query 'authorizationData[].authorizationToken')
+        #curl -i -H "Authorization: Basic $TOKEN" https://"${REGISTRY}"/v2/amazonlinux/tags/list
+        aws ecr get-login-password --region "${REGION}" | docker login -u AWS "${REGISTRY}" --password-stdin
+        #aws ecr get-login-password --region "${REGION}" | docker login -u AWS "${REPO}" -p
+}
+
+if ! grep -q "${REGISTRY}" ~/.docker/config.json ; then
+	dockerlogin
+fi
+
+if ! docker pull $IMAGE; then
+	docker logout "${REGISTRY}"
+	dockerlogin
+	docker pull $IMAGE || echo "Failed to pull docker image $IMAGE"
+fi
 
 if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
 	echo "Stopping escrow-dashboard container"
