acff1f88fc0b827a157aafe7522eacf8172285ea
makarofalex3 <alek@hmt.ai>
2022-06-09 14:11:18 +0300

    added structures

---
+++ src/components/Skeletons/SkeletonHmTransfer.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
+
+export const SkeletonHmTransfer: React.FC = (): React.ReactElement => {
+  return (
+    <>
+      <Stack spacing={1}>
+        <Skeleton variant="text" />
+        <Skeleton variant="circular" width={40} height={40} />
+        <Skeleton variant="circular" width={40} height={40} />
+        <Skeleton variant="circular" width={40} height={40} />
+        <Skeleton variant="circular" width={40} height={40} />
+
+        <Skeleton variant="rectangular" width={410} height={218} />
+      </Stack>
+    </>
+  );
+};
