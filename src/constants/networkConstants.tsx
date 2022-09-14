075f9a1646a6fec0f06ddab0e6aaf8d939cdf3e4
Ivan <ivan@hmt.ai>
2022-09-14 19:50:58 +0300

    Add BSC networks

--- src/constants/networkConstants.tsx
+++ src/constants/networkConstants.tsx
@@ -23,6 +23,17 @@ export const networkMap: INetworkMap = {
       'https://api.thegraph.com/subgraphs/name/humanprotocol/polygon',
     showTokenStats: true,
   },
+  bsc: {
+    title: 'Binance Smart Chain Mainnet',
+    key: 'bsc',
+    scanner: 'https://bscscan.com',
+    rpcUrl: 'https://bsc-dataseed1.binance.org/',
+    defaultFactoryAddr: '0xc88bC422cAAb2ac8812de03176402dbcA09533f4',
+    hmtAddr: '0x0d501B743F22b641B8C8dfe00F1AAb881D57DDC7',
+    graphqlClientUrl:
+      'https://api.thegraph.com/subgraphs/name/humanprotocol/bsc',
+    showTokenStats: true,
+  },
   mumbai: {
     title: 'Polygon Mumbai Testnet',
     key: 'mumbai',
@@ -53,6 +64,28 @@ export const networkMap: INetworkMap = {
     rpcUrl: 'https://proxy.devnet.neonlabs.org/solana',
     defaultFactoryAddr: '0x75D377773aCf9eB1076B01c1698415Bfe2db6D9d',
     hmtAddr: '',
+    graphqlClientUrl: 'https://api.thegraph.com',
+    // the graph is not implemented yet
+    showTokenStats: false,
+  },
+  moonbeam: {
+    title: 'Moonbeam Mainnet',
+    key: 'moonbeam',
+    scanner: 'https://moonbeam.moonscan.io',
+    rpcUrl: 'https://rpc.api.moonbeam.network',
+    defaultFactoryAddr: '0x98108c28B7767a52BE38B4860832dd4e11A7ecad',
+    hmtAddr: '0x3b25BC1dC591D24d60560d0135D6750A561D4764',
+    graphqlClientUrl:
+      'https://api.thegraph.com/subgraphs/name/humanprotocol/moonbeam',
+    showTokenStats: true,
+  },
+  bsctest: {
+    title: 'Binance Smart Chain Testnet',
+    key: 'bsctest',
+    scanner: 'https://testnet.bscscan.com',
+    rpcUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+    defaultFactoryAddr: '0x161228DaAB6F935548E2B98f6cedC9FF648C781d',
+    hmtAddr: '0x1f613bc2f4e741EA96d30A5957fF7fA6916acDD6',
     graphqlClientUrl: 'https://api.thegraph.com',
     // the graph is not implemented yet
     showTokenStats: false,
